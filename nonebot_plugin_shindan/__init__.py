52ea5a1bf27277c3d0d05e0d8acc68524227fb84
meetwq <meetwq@gmail.com>
2022-06-18 00:13:58 +0800

    format code

--- nonebot_plugin_shindan/__init__.py
+++ nonebot_plugin_shindan/__init__.py
@@ -18,16 +18,16 @@ from .shindan_list import add_shindan, del_shindan, set_shindan, get_shindan_lis
 from .shindanmaker import make_shindan, get_shindan_title
 
 
-__help__plugin_name__ = 'shindan'
-__des__ = 'shindanmaker趣味占卜'
-__cmd__ = '''
+__help__plugin_name__ = "shindan"
+__des__ = "shindanmaker趣味占卜"
+__cmd__ = """
 发送“占卜列表”查看可用占卜
 发送“{占卜名} {名字}”使用占卜
-'''.strip()
-__example__ = '''
+""".strip()
+__example__ = """
 人设生成 小Q
-'''.strip()
-__usage__ = f'{__des__}\nUsage:\n{__cmd__}\nExample:\n{__example__}'
+""".strip()
+__usage__ = f"{__des__}\nUsage:\n{__cmd__}\nExample:\n{__example__}"
 
 
 add_usage = """Usage:
@@ -44,12 +44,12 @@ set_usage = """Usage:
 如：设置占卜 360578 text"""
 
 cmd_sd = on_command(
-    '占卜', aliases={'shindan', 'shindanmaker'}, rule=to_me(), block=True, priority=8
+    "占卜", aliases={"shindan", "shindanmaker"}, rule=to_me(), block=True, priority=13
 )
-cmd_ls = on_command('占卜列表', aliases={'可用占卜'}, block=True, priority=8)
-cmd_add = on_command('添加占卜', permission=SUPERUSER, block=True, priority=8)
-cmd_del = on_command('删除占卜', permission=SUPERUSER, block=True, priority=8)
-cmd_set = on_command('设置占卜', permission=SUPERUSER, block=True, priority=8)
+cmd_ls = on_command("占卜列表", aliases={"可用占卜"}, block=True, priority=13)
+cmd_add = on_command("添加占卜", permission=SUPERUSER, block=True, priority=13)
+cmd_del = on_command("删除占卜", permission=SUPERUSER, block=True, priority=13)
+cmd_set = on_command("设置占卜", permission=SUPERUSER, block=True, priority=13)
 
 
 @cmd_sd.handle()
@@ -62,11 +62,11 @@ async def _():
     sd_list = get_shindan_list()
 
     if not sd_list:
-        await cmd_ls.finish('尚未添加任何占卜')
+        await cmd_ls.finish("尚未添加任何占卜")
 
     await cmd_ls.finish(
-        f'可用占卜：\n'
-        + '\n'.join([f"{s['command']}（{s['title']}）" for s in sd_list.values()])
+        f"可用占卜：\n"
+        + "\n".join([f"{s['command']}（{s['title']}）" for s in sd_list.values()])
     )
 
 
@@ -84,16 +84,16 @@ async def _(msg: Message = CommandArg()):
     command = args[1]
     title = await get_shindan_title(id)
     if not title:
-        await cmd_add.finish('找不到该占卜，请检查id')
+        await cmd_add.finish("找不到该占卜，请检查id")
 
     sd_list = get_shindan_list()
     if id in sd_list:
-        await cmd_add.finish('该占卜已存在')
+        await cmd_add.finish("该占卜已存在")
     if command in sd_list.values():
-        await cmd_add.finish('该指令已存在')
+        await cmd_add.finish("该指令已存在")
 
     if add_shindan(id, command, title):
-        await cmd_add.finish(f'成功添加占卜“{title}”，可通过“{command} 名字”使用')
+        await cmd_add.finish(f"成功添加占卜“{title}”，可通过“{command} 名字”使用")
 
 
 @cmd_del.handle()
@@ -108,10 +108,10 @@ async def _(msg: Message = CommandArg()):
     id = arg
     sd_list = get_shindan_list()
     if id not in sd_list:
-        await cmd_del.finish('不存在该占卜')
+        await cmd_del.finish("不存在该占卜")
 
     if del_shindan(id):
-        await cmd_del.finish('成功删除该占卜')
+        await cmd_del.finish("成功删除该占卜")
 
 
 @cmd_set.handle()
@@ -121,17 +121,17 @@ async def _(msg: Message = CommandArg()):
         await cmd_set.finish(set_usage)
 
     args = arg.split()
-    if len(args) != 2 or not args[0].isdigit() or args[1] not in ['text', 'image']:
+    if len(args) != 2 or not args[0].isdigit() or args[1] not in ["text", "image"]:
         await cmd_set.finish(set_usage)
 
     id = args[0]
     mode = args[1]
     sd_list = get_shindan_list()
     if id not in sd_list:
-        await cmd_set.finish('不存在该占卜')
+        await cmd_set.finish("不存在该占卜")
 
     if set_shindan(id, mode):
-        await cmd_set.finish('设置成功')
+        await cmd_set.finish("设置成功")
 
 
 def sd_handler() -> Rule:
@@ -143,32 +143,32 @@ def sd_handler() -> Rule:
         state: T_State = State(),
     ) -> bool:
         async def get_name(command: str) -> str:
-            name = ''
+            name = ""
             for msg_seg in msg:
-                if msg_seg.type == 'at':
+                if msg_seg.type == "at":
                     assert isinstance(event, GroupMessageEvent)
                     info = await bot.get_group_member_info(
-                        group_id=event.group_id, user_id=msg_seg.data['qq']
+                        group_id=event.group_id, user_id=msg_seg.data["qq"]
                     )
-                    name = info.get('card', '') or info.get('nickname', '')
+                    name = info.get("card", "") or info.get("nickname", "")
                     break
             if not name:
                 name = msg_text[len(command) :].strip()
             if not name:
                 name = event.sender.card or event.sender.nickname
-            return name
+            return name or ""
 
         sd_list = get_shindan_list()
         sd_list = sorted(
-            sd_list.items(), reverse=True, key=lambda items: items[1]['command']
+            sd_list.items(), reverse=True, key=lambda items: items[1]["command"]
         )
         for id, s in sd_list:
-            command = s['command']
+            command = s["command"]
             if msg_text.startswith(command):
                 name = await get_name(command)
-                state['id'] = id
-                state['name'] = name
-                state['mode'] = s.get('mode', 'image')
+                state["id"] = id
+                state["name"] = name
+                state["mode"] = s.get("mode", "image")
                 return True
         return False
 
@@ -180,17 +180,17 @@ sd_matcher = on_message(sd_handler(), priority=9)
 
 @sd_matcher.handle()
 async def _(state: T_State = State()):
-    id = state.get('id')
-    name = state.get('name')
-    mode = state.get('mode')
+    id: str = state["id"]
+    name: str = state["name"]
+    mode: str = state["mode"]
     try:
         res = await make_shindan(id, name, mode)
     except:
         logger.warning(traceback.format_exc())
-        await sd_matcher.finish('出错了，请稍后再试')
+        await sd_matcher.finish("出错了，请稍后再试")
 
     if isinstance(res, str):
-        img_pattern = r'((?:http|https)://\S+\.(?:jpg|jpeg|png|gif|bmp|webp))'
+        img_pattern = r"((?:http|https)://\S+\.(?:jpg|jpeg|png|gif|bmp|webp))"
         message = Message()
         msgs = re.split(img_pattern, res)
         for msg in msgs:
--- nonebot_plugin_shindan/shindanmaker.py
+++ nonebot_plugin_shindan/shindanmaker.py
@@ -3,13 +3,13 @@ import time
 import httpx
 import jinja2
 from pathlib import Path
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from typing import Tuple, Union
 
 from nonebot_plugin_htmlrender import html_to_pic
 
 
-tpl_path = Path(__file__).parent / 'templates'
+tpl_path = Path(__file__).parent / "templates"
 env = jinja2.Environment(loader=jinja2.FileSystemLoader(tpl_path), enable_async=True)
 
 
@@ -20,7 +20,7 @@ def retry(func):
                 return await func(*args, **kwargs)
             except:
                 continue
-        raise Exception('网络错误')
+        raise
 
     return wrapper
 
@@ -32,61 +32,61 @@ headers = {
 
 @retry
 async def get(client: httpx.AsyncClient, url: str, **kwargs):
-    return await client.get(url, headers=headers, timeout=10, **kwargs)
+    return await client.get(url, headers=headers, timeout=20, **kwargs)
 
 
 @retry
 async def post(client: httpx.AsyncClient, url: str, **kwargs):
-    return await client.post(url, headers=headers, timeout=10, **kwargs)
+    return await client.post(url, headers=headers, timeout=20, **kwargs)
 
 
-async def get_shindan_title(id: int) -> str:
-    url = f'https://shindanmaker.com/{id}'
-    try:
-        async with httpx.AsyncClient() as client:
-            resp = await get(client, url)
-            dom = BeautifulSoup(resp.text, 'lxml')
-            return dom.find('h1', {'id': 'shindanTitle'}).text
-    except:
-        return ''
+async def get_shindan_title(id: str) -> str:
+    url = f"https://shindanmaker.com/{id}"
+    async with httpx.AsyncClient() as client:
+        resp = await get(client, url)
+        dom = BeautifulSoup(resp.text, "lxml")
+        title = dom.find("h1", {"id": "shindanTitle"})
+        assert title
+        return title.text
 
 
-async def make_shindan(id: str, name: str, mode='image') -> Union[str, bytes]:
-    url = f'https://shindanmaker.com/{id}'
+async def make_shindan(id: str, name: str, mode="image") -> Union[str, bytes]:
+    url = f"https://shindanmaker.com/{id}"
     seed = time.strftime("%y%m%d", time.localtime())
     async with httpx.AsyncClient() as client:
         resp = await get(client, url)
-        dom = BeautifulSoup(resp.text, 'lxml')
-        token = dom.find('form', {'id': 'shindanForm'}).find('input')['value']
-        payload = {'_token': token, 'shindanName': name + seed, 'hiddenName': '名無しのR'}
+        dom = BeautifulSoup(resp.text, "lxml")
+        token = dom.find("form", {"id": "shindanForm"}).find("input")["value"]  # type: ignore
+        payload = {"_token": token, "shindanName": name + seed, "hiddenName": "名無しのR"}
         resp = await post(client, url, json=payload)
 
     content = resp.text
-    if mode == 'image':
+    if mode == "image":
         html, has_chart = await render_html(content)
         html = html.replace(name + seed, name)
         return await html_to_pic(
             html,
-            template_path=f'file://{tpl_path.absolute()}',
+            template_path=f"file://{tpl_path.absolute()}",
             wait=2000 if has_chart else 0,
-            viewport={'width': 800, 'height': 100},
+            viewport={"width": 800, "height": 100},
         )
     else:
-        dom = BeautifulSoup(content, 'lxml')
-        result = dom.find('span', {'id': 'shindanResult'})
-        for img in result.find_all('img'):
-            img.replace_with(img['src'])
+        dom = BeautifulSoup(content, "lxml")
+        result = dom.find("span", {"id": "shindanResult"})
+        assert isinstance(result, Tag)
+        for img in result.find_all("img"):
+            img.replace_with(img["src"])
         return result.text.replace(name + seed, name)
 
 
 async def render_html(content: str) -> Tuple[str, bool]:
-    dom = BeautifulSoup(content, 'lxml')
-    result_js = str(dom.find('script', string=re.compile(r'saveResult')))
-    title = str(dom.find('div', class_='shindanTitleDescBlock'))
-    result = str(dom.find('div', {'id': 'shindanResultBlock'}))
-    has_chart = 'chart.js' in content
+    dom = BeautifulSoup(content, "lxml")
+    result_js = str(dom.find("script", string=re.compile(r"saveResult")))
+    title = str(dom.find("div", class_="shindanTitleDescBlock"))
+    result = str(dom.find("div", {"id": "shindanResultBlock"}))
+    has_chart = "chart.js" in content
 
-    shindan_tpl = env.get_template('shindan.html')
+    shindan_tpl = env.get_template("shindan.html")
     html = await shindan_tpl.render_async(
         result_js=result_js, title=title, result=result, has_chart=has_chart
     )
