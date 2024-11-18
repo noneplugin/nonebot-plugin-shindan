a959435cff79de0ba3eb24fe5ea57eb57ad45d93
meetwq <meetwq@gmail.com>
2024-11-18 17:46:13 +0800

    use browser to get content

--- nonebot_plugin_shindan/shindanmaker.py
+++ nonebot_plugin_shindan/shindanmaker.py
@@ -6,7 +6,7 @@ from typing import Union
 import httpx
 import jinja2
 from bs4 import BeautifulSoup, Tag
-from nonebot_plugin_htmlrender import html_to_pic
+from nonebot_plugin_htmlrender import get_new_page, html_to_pic
 
 from .config import shindan_config
 from .model import ShindanConfig
@@ -15,69 +15,38 @@ tpl_path = Path(__file__).parent / "templates"
 env = jinja2.Environment(loader=jinja2.FileSystemLoader(tpl_path), enable_async=True)
 
 
-headers = {
-    "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/96.0.4664.110 Safari/537.36"
-    )
-}
-
+USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/96.0.4664.110 Safari/537.36"
+)
+headers = {"user-agent": USER_AGENT}
 if shindan_config.shindanmaker_cookie:
     headers["cookie"] = shindan_config.shindanmaker_cookie
 
 
-async def request(client: httpx.AsyncClient, method: str, url: str, **kwargs):
-    resp = await client.request(
-        method, url, headers=headers, timeout=20, follow_redirects=True, **kwargs
-    )
-    resp.raise_for_status()
-    return resp
-
-
-async def get(client: httpx.AsyncClient, url: str, **kwargs):
-    return await request(client, "GET", url, **kwargs)
-
-
-async def post(client: httpx.AsyncClient, url: str, **kwargs):
-    return await request(client, "POST", url, **kwargs)
-
-
 async def download_image(url: str) -> bytes:
     async with httpx.AsyncClient() as client:
-        resp = await get(client, url)
+        resp = await client.get(url, headers=headers, timeout=20, follow_redirects=True)
         return resp.read()
 
 
 async def get_shindan_title(id: int) -> str:
     url = f"https://shindanmaker.com/{id}"
-    async with httpx.AsyncClient() as client:
-        resp = await get(client, url)
-        dom = BeautifulSoup(resp.text, "lxml")
-        title = dom.find("h1", {"id": "shindanTitle"})
-        assert title
-        return title.text
+    async with get_new_page(user_agent=USER_AGENT) as page:
+        await page.goto(url, wait_until="commit")
+        return await page.locator('//h1[@id="shindanTitle"]').inner_text()
 
 
 async def make_shindan(id: int, name: str, mode="image") -> Union[str, bytes]:
     url = f"https://shindanmaker.com/{id}"
     seed = time.strftime("%y%m%d", time.localtime())
-    async with httpx.AsyncClient() as client:
-        resp = await get(client, url)
-        dom = BeautifulSoup(resp.text, "lxml")
-        form = dom.find("form", {"id": "shindanForm"})
-        _token = form.find("input", {"name": "_token"})["value"]  # type: ignore
-        # shindan_token = form.find("input", {"name": "shindan_token"})["value"]
-        payload = {
-            "_token": _token,
-            "user_input_value_1": name + seed,
-            "randname": "名無しのR",
-            "type": "name",
-            # "shindan_token": shindan_token,
-        }
-        resp = await post(client, url, json=payload)
+    async with get_new_page(user_agent=USER_AGENT) as page:
+        await page.goto(url, wait_until="commit")
+        await page.locator('//input[@id="user_input_value_1"]').fill(name + seed)
+        await page.locator('//button[@id="shindanButtonSubmit"]').click()
+        content = await page.content()
 
-    content = resp.text
     if mode == "image":
         html, has_chart = await render_html(content)
         html = html.replace(seed, "")
